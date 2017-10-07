d53a78fc4d6983b63aa25234484ed7d97dd30bd1
Gavin Benedict <gavbenedict@gmail.com>
2017-10-07 10:58:07 -0700

    completing part 2

--- ex02.exs
+++ ex02.exs
@@ -1,6 +1,31 @@
 
 defmodule Ex02 do
+  def new_counter(value \\ 0) do
+      {:ok, counter} = Agent.start_link(Counter, :new, [value])
+      counter
+  end
+  
+  def next_value(counter) do
+    Agent.get_and_update(counter, Counter, :next_value, [])
+  end
+  
+  def new_global_counter() do
+    {:ok, counter} = Agent.start_link(&Counter.new/0, name: Counter)
+    counter
+  end
 
+  def global_next_value() do
+    Agent.get_and_update(Counter, &Counter.next_value/1)
+  end
+end
+
+defmodule Counter do
+  def new(value \\ 0) do
+    value
+  end
+  def next_value(state) do
+    {state, state + 1}
+  end
 end
 
 ExUnit.start()
@@ -32,26 +57,26 @@ defmodule Test do
   Replace the placeholders with your code.
   """
 
-  # test "counter using an agent" do
-  #   { :ok, counter } = « your code »
-  # 
-  #   value   = « your code »
-  #   assert value == 0
-  # 
-  #   value   = « your code »
-  #   assert value == 1
-  # end
+  test "counter using an agent" do
+    { :ok, counter } = Agent.start_link(Counter, :new, [])
+  
+    value   = Agent.get_and_update(counter, Counter, :next_value, [])
+    assert value == 0
+  
+    value   = Agent.get_and_update(counter, Counter, :next_value, [])
+    assert value == 1
+  end
 
   @doc """
   Next, uncomment this test, and add code to the Ex02 module at the
   top of this file to make those tests run.
   """
 
-  # test "higher level API interface" do
-  #   count = Ex02.new_counter(5)
-  #   assert  Ex02.next_value(count) == 5
-  #   assert  Ex02.next_value(count) == 6
-  # end
+  test "higher level API interface" do
+    count = Ex02.new_counter(5)
+    assert  Ex02.next_value(count) == 5
+    assert  Ex02.next_value(count) == 6
+  end
 
   @doc """
   Last (for this exercise), we'll create a global counter by adding
@@ -60,12 +85,12 @@ defmodule Test do
   that agent into calls to `global_next_value`?
   """
 
-  # test "global counter" do
-  #   Ex02.new_global_counter
-  #   assert Ex02.global_next_value == 0
-  #   assert Ex02.global_next_value == 1
-  #   assert Ex02.global_next_value == 2
-  # end
+  test "global counter" do
+    Ex02.new_global_counter
+    assert Ex02.global_next_value == 0
+    assert Ex02.global_next_value == 1
+    assert Ex02.global_next_value == 2
+  end
 end
 
 
