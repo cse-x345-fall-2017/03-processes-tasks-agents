f2bb0b53038f2e888f5418643bad457f5b16c5bf
Michael Gilbert <euphoricpoptarts@gmail.com>
2017-10-15 22:50:10 -0500

    hw3 commit

--- ex02.exs
+++ ex02.exs
@@ -1,5 +1,43 @@
 
+defmodule CounterServer do
+ 
+ def init(_args) do
+    { :ok, 0 }
+ end
+
+ def handle_cast({ :set, value }, _state) do
+    { :noreply, value }
+ end
+
+ def handle_call({ :get }, _from, state) do
+    { :reply, state, state }
+ end
+
+end
+
 defmodule Ex02 do
+
+  @server CounterGlobal
+
+  def new_counter(value) do
+    { :ok, counter } = Agent.start(fn -> value end)
+    counter
+  end
+
+  def new_global_counter do
+    GenServer.start(CounterServer, [], name: @server)
+  end
+
+  def global_next_value do
+    current = GenServer.call(@server, { :get })
+    GenServer.cast(@server, { :set, current + 1 })
+    current
+  end
+
+
+  def next_value(counter) do
+    Agent.get_and_update(counter, fn state -> {state, state + 1} end)
+  end
 
 end
 
@@ -32,26 +70,26 @@ defmodule Test do
   Replace the placeholders with your code.
   """
 
-  # test "counter using an agent" do
-  #   { :ok, counter } = « your code »
-  # 
-  #   value   = « your code »
-  #   assert value == 0
-  # 
-  #   value   = « your code »
-  #   assert value == 1
-  # end
+   test "counter using an agent" do
+     { :ok, counter } = Agent.start(fn -> 0 end)
+   
+     value   = Agent.get_and_update(counter,fn state -> {state, state + 1} end)
+     assert value == 0
+   
+     value   = Agent.get_and_update(counter,fn state -> {state, state + 1} end)
+     assert value == 1
+   end
 
   @doc """
   Next, uncomment this test, and add code to the Ex02 module at the
   top of this file to make those tests run.
   """
 
-  # test "higher level API interface" do
-  #   count = Ex02.new_counter(5)
-  #   assert  Ex02.next_value(count) == 5
-  #   assert  Ex02.next_value(count) == 6
-  # end
+   test "higher level API interface" do
+     count = Ex02.new_counter(5)
+     assert  Ex02.next_value(count) == 5
+     assert  Ex02.next_value(count) == 6
+   end
 
   @doc """
   Last (for this exercise), we'll create a global counter by adding
@@ -60,12 +98,12 @@ defmodule Test do
   that agent into calls to `global_next_value`?
   """
 
-  # test "global counter" do
-  #   Ex02.new_global_counter
-  #   assert Ex02.global_next_value == 0
-  #   assert Ex02.global_next_value == 1
-  #   assert Ex02.global_next_value == 2
-  # end
+   test "global counter" do
+     Ex02.new_global_counter
+     assert Ex02.global_next_value == 0
+     assert Ex02.global_next_value == 1
+     assert Ex02.global_next_value == 2
+   end
 end
 
 
